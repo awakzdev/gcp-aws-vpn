d3a6e79112e35abcc5310241d19a04ac4ec5016c
Elazar <96201125+awakzdev@users.noreply.github.com>
2023-08-02 10:20:33 +0300

    Changed to lowercase

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-output "GCP_Reserved_IP" { value = google_compute_address.vpn_static_ip.address }
-output "Tunnel-Status-2" { value = google_compute_vpn_tunnel.vpn-1.detailed_status }
-output "Tunnel-Status-1" { value = google_compute_vpn_tunnel.vpn-2.detailed_status }
-output "GCP_Instance_IP" { value = [for k in google_compute_instance.default.network_interface : k.network_ip] }
-output "AWS_Instance_IP" { value = aws_instance.ec2.public_ip }
-output "AWS_VPC_CIDR" { value = aws_vpc.network.cidr_block }
+output "gcp_reserved_ip" { value = google_compute_address.vpn_static_ip.address }
+output "tunnel-status-2" { value = google_compute_vpn_tunnel.vpn-1.detailed_status }
+output "tunnel-status-1" { value = google_compute_vpn_tunnel.vpn-2.detailed_status }
+output "gcp_instance_ip" { value = [for k in google_compute_instance.default.network_interface : k.network_ip] }
+output "aws_instance_ip" { value = aws_instance.ec2.public_ip }
+output "aws_vpc_cidr" { value = aws_vpc.network.cidr_block }
